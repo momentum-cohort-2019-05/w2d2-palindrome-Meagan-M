d520a9977ed061957d361d5086d86d6c6c8c1353
Meagan-M <meaganmakinson80@gmail.com>
2019-06-04 12:37:16 -0400

    add palindrome assignment

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+# You have to write a program that, when run, asks the user to input some text. It can be a phrase, a sentence, or multiple sentences. After it is entered, your program will let the user know if it is a palindrome or not. Use "is a palindrome" and "is not a palindrome" in your output in order for the tests to pass.
+
+# Letter casing and punctuation do not matter when testing a palindrome. All of the following are valid palindromes:
+
+# stunt nuts
+# Lisa Bonet ate no basil.
+# A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal: Panama!
+# Doc, note, I dissent. A fast never prevents a fatness. I diet on cod.
+
+
